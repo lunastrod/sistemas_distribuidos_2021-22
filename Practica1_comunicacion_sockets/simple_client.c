21555e1c22f372a6ede94047bee8ecaa62c29c83
dparras <daniel.parra.segovia@gmail.com>
2021-09-22 10:06:00 +0200

    socket bind

--- Practica1_comunicacion_sockets/simple_client.c
+++ Practica1_comunicacion_sockets/simple_client.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+
+
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   return 0;
+    // printf() displays the string inside quotation
+    printf("Hello, World!\n");
+
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    int my_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    int res = bind( my_socket,
+                (struct sockaddr *) &servaddr,
+                sizeof(servaddr)) == -1 ;
+    
+    return 0;
 }
+
