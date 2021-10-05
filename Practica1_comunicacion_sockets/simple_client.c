2789b1a35b1af4f508c31f7a79b92881e73d54b7
danparseg <daniel.parra.segovia@gmail.com>
2021-10-05 16:57:46 +0200

    mas o menos funciona

--- Practica1_comunicacion_sockets/simple_client.c
+++ Practica1_comunicacion_sockets/simple_client.c
@@ -2,7 +2,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define PORT 8080
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <signal.h>
+
+#include <arpa/inet.h>//temp
 
 /*
 socket
@@ -14,6 +21,7 @@ send
 close
 */
 
+/*
 int main() {
     printf("client\n");
 
@@ -29,4 +37,79 @@ int main() {
     
     return 0;
 }
+*/
 
+
+
+
+#define MAX_BUFF 80
+#define PORT 8080
+#define SA struct sockaddr
+
+static volatile int running = 1;
+
+void intHandler(int sig) {
+    running = 0;
+}
+
+
+void sendRecv(int sockfd)
+{
+    char buff[MAX_BUFF];
+
+    while (running) {
+        bzero(buff, sizeof(buff));
+        printf(">");
+
+        for(int i=0; i<MAX_BUFF; i++){
+            char in=getchar();
+            if(in=='\n'){
+                break;
+            }
+            buff[i]=in;
+        }
+
+        send(sockfd, buff, sizeof(buff), 0);
+        bzero(buff, sizeof(buff));
+        recv(sockfd, buff, sizeof(buff),0);
+        printf("+++%s\n", buff);
+
+        signal(SIGINT, intHandler);
+    }
+    printf("DEBUG:exit");
+}
+   
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+   
+    // socket create and varification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+   
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+   
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+   
+    // function for chat
+    sendRecv(sockfd);
+   
+    // close the socket
+    close(sockfd);
+}
--- Practica1_comunicacion_sockets/simple_server.c
+++ Practica1_comunicacion_sockets/simple_server.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
-#define PORT 8080
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+
+//#define PORT 8080
 
 /*
-
-
-
-
 socket
 bind
 listen
@@ -19,7 +23,7 @@ send
 
 close
 */
-
+/*
 int main() {
     //int server_fd;
 
@@ -47,4 +51,96 @@ int main() {
 
     
     return 0;
+}
+*/
+
+
+#define MAX_BUFF 80
+#define PORT 8080
+#define SA struct sockaddr
+
+// Function designed for chat between client and server.
+
+static volatile int running = 1;
+
+void intHandler(int sig) {
+    running = 0;
+}
+
+void sendRecv(int sockfd)
+{
+    char buff[MAX_BUFF];
+    while (running){
+        bzero(buff, sizeof(buff));
+        printf(">");
+        recv(sockfd, buff, sizeof(buff),0);
+        printf("+++%s\n", buff);
+
+
+        bzero(buff, sizeof(buff));
+        printf(">");
+
+        for(int i=0; i<MAX_BUFF; i++){
+            char in=getchar();
+            if(in=='\n'){
+                break;
+            }
+            buff[i]=in;
+        }
+
+        send(sockfd, buff, sizeof(buff), 0);
+        signal(SIGINT, intHandler);
+    }
+}
+   
+int main(){
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+   
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+   
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+   
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
+   
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+    len = sizeof(cli);
+   
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server accept the client...\n");
+   
+    // Function for chatting between client and server
+    sendRecv(connfd);
+   
+    // After chatting close the socket
+    close(sockfd);
 }
