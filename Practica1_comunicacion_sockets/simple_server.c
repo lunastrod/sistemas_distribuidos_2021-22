5f1eea20ced4e5690401111c97d4eff43b40f0af
danparseg <daniel.parra.segovia@gmail.com>
2021-10-05 17:08:59 +0200

    creo que los simples

--- Practica1_comunicacion_sockets/simple_server.c
+++ Practica1_comunicacion_sockets/simple_server.c
@@ -91,45 +91,41 @@ void sendRecv(int sockfd)
         send(sockfd, buff, sizeof(buff), 0);
         signal(SIGINT, intHandler);
     }
+    printf("DEBUG:exit");
 }
    
 int main(){
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
    
-    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
-        printf("socket creation failed...\n");
+        printf("socket creation failed\n");
         exit(0);
     }
     else
-        printf("Socket successfully created..\n");
+        printf("Socket successfully created...\n");
     bzero(&servaddr, sizeof(servaddr));
    
-    // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
    
-    // Binding newly created socket to given IP and verification
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
+        printf("socket bind failed\n");
         exit(0);
     }
     else
         printf("Socket successfully binded..\n");
    
-    // Now server is ready to listen and verification
     if ((listen(sockfd, 5)) != 0) {
-        printf("Listen failed...\n");
+        printf("Listen failed\n");
         exit(0);
     }
     else
-        printf("Server listening..\n");
+        printf("Server listening...\n");
     len = sizeof(cli);
    
-    // Accept the data packet from client and verification
     connfd = accept(sockfd, (SA*)&cli, &len);
     if (connfd < 0) {
         printf("server accept failed...\n");
@@ -138,9 +134,7 @@ int main(){
     else
         printf("server accept the client...\n");
    
-    // Function for chatting between client and server
     sendRecv(connfd);
    
-    // After chatting close the socket
     close(sockfd);
 }
