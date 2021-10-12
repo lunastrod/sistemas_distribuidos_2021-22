d7fb4f72dcbea2533ac719d64bcf5be89a6c7f69
danparseg <daniel.parra.segovia@gmail.com>
2021-10-12 14:15:32 +0200

    .

--- Practica2_relojes/proxy.h
+++ Practica2_relojes/proxy.h
@@ -1,3 +1,23 @@
+enum operations {
+    READY_TO_SHUTDOWN = 0,
+    SHUTDOWN_NOW,
+    SHUTDOWN_ACK
+};
+
+/*
+El campo origin contendrá el nombre del proceso que envía el mensaje.
+El campo action podrá contener valores del enumerado operations:
+· READY_TO_SHUTDOWN: la máquina notifica que está lista para apagarse.
+· SHUTDOWN_NOW: Al recibir este mensaje la máquina sabe que debe apagarse.
+· SHUTDOWN_ACK: La máquina manda este mensaje antes justo de realizar el shutdown.
+El campo clock_lamport es utilizado para enviar el contador de lamport.
+*/
+struct message {
+    char origin[20];
+    enum operations action;
+    unsigned int clock_lamport;
+};
+
 // Establece el nombre del proceso (para los logs y trazas)
 void set_name (char name[2]);
 // Establecer ip y puerto
@@ -9,5 +29,3 @@ int get_clock_lamport();
 void notify_ready_shutdown();
 // Notifica que va a realizar el shutdown correctamente (SHUTDOWN_ACK)
 void notify_shutdown_ack();
-
-void hello();
