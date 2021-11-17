cbb0a4a0fbef0f2659b36df4c3c682ca2762b165
danparseg <daniel.parra.segovia@gmail.com>
2021-11-17 18:49:48 +0100

    uff

--- Practica3_concurrencia/test.c
+++ Practica3_concurrencia/test.c
@@ -3,45 +3,52 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define NUM_THREADS 100
+#define NUM_THREADS 10
 #define MAX_COUNT 100000
+
+
+#define RATIO_N 50
+int ratio=0;//boolean, if(ratio)
+int ratio_counter=0;
+sem_t ratio_mutex;//acceder a la variable ratio_counter
+sem_t ratio_sem;
 
 long counter=0;
 sem_t write_sem;
 sem_t read_sem;
 sem_t new_readers_sem;
-sem_t ratio_sem;
 
 int readers=0;
 int writers=0;
-int ratio=0;
-sem_t readers_mutex;
-sem_t writers_mutex;
+sem_t readers_mutex;//acceder a la variable readers
+sem_t writers_mutex;//acceder a la variable writers
 
 void read_rp(int id){
-    //reader needs mutually exclusive access while
-    //manipulating “readers” variable
+
     //fprintf(stderr,"P%d quiero leer ratio_sem\n",id);
     //sem_wait(&ratio_sem);
     fprintf(stderr,"P%d quiero leer readers_mutex\n",id);
     sem_wait(&readers_mutex);
-    readers++;
-    ratio++;
-    if(readers==1){
-        //If this reader is the first reader, it has to 
-        //wait if there is an active writer (which has
-        //exclusive access to the database)
-        fprintf(stderr,"P%d quiero leer write_sem\n",id);
-        sem_wait(&write_sem);
-        //If other readers come along while the first 
-        //one is waiting, they wait at the sem_wait(&readers_mutex);
-    }
-    //reader does not need mutually exclusive access 
-    //while reading database
+        readers++;
+        ratio++;
+        if(readers==1){
+            fprintf(stderr,"P%d quiero leer write_sem\n",id);
+            sem_wait(&write_sem);
+        }
+
+        fprintf(stderr,"P%d quiero leer modifico ratio_counter,%d\n",id,ratio_counter);
+        sem_wait(&ratio_mutex);
+        ratio_counter++;
+        sem_post(&ratio_mutex);
+
+        if(ratio && ratio_counter>RATIO_N){
+            sem_post(&write_sem);
+            sem_wait(&ratio_sem);
+        }
     sem_post(&readers_mutex);
     //sem_post(&ratio_sem);
 
-    sleep(1);
+    //sleep(1);
     printf("P%d counter:%ld\n",id,counter);
 
     sem_wait(&readers_mutex);
@@ -61,8 +68,16 @@ void write_rp(int id){
     //sem_wait(&ratio_sem);
     fprintf(stderr,"P%d quiero escribir write_sem\n",id);
     sem_wait(&write_sem);
-    sleep(1);
-    ratio=0;
+
+    //sleep(1);
+
+    sem_wait(&ratio_mutex);
+    fprintf(stderr,"P%d quiero escribir me dejan pasar con ratio=%d\n",id,ratio_counter);
+    ratio_counter=0;
+    sem_post(&ratio_mutex);
+
+    sem_post(&ratio_sem);
+
     counter++;
     printf("P%d counter++:%ld\n",id,counter);
     sem_post(&write_sem);
@@ -85,7 +100,7 @@ void read_wp(int id){
     sem_post(&read_sem);
     sem_post(&new_readers_sem);
 
-    sleep(1);
+    //sleep(1);
     printf("P%d counter:%ld\n",id,counter);
 
     sem_wait(&readers_mutex);
@@ -124,15 +139,17 @@ void *count(void *ptr) {
     int id=(int)ptr;
     //if(id==13||id==25||id==79){
     
-    if(id==20){
-        //while(1){
-            write_wp(id);
-        //}
+    if(id==0){
+        while(1){
+            write_rp(id);
+            //sleep(1);
+        }
     }
     else{
-        //while(1){
-            read_wp(id);
-        //}
+        while(1){
+            read_rp(id);
+            //sleep(1);
+        }
         
     }
     return NULL;
@@ -146,6 +163,7 @@ int main (int argc, char* argv[]) {
     sem_init(&writers_mutex, 0, 1);
     sem_init(&new_readers_sem, 0, 1);
     sem_init(&ratio_sem,0,1);
+    sem_init(&ratio_mutex,0,1);
 
     for (int i=0; i<NUM_THREADS; i++){
         pthread_create(&threads[i],NULL, count, (void *)i);
